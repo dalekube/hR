a3497eac56b5ab35b32a39ece01dcde0c3b13419
dalekube <dkube@uwalumni.com>
2017-11-26 19:31:09 -0600

    Simplified function by removing dplyr dependency

--- R/hierarchyWide.R
+++ R/hierarchyWide.R
@@ -2,18 +2,18 @@
 #' @description This function takes two lists representing employee and supervisor
 #' identifiers (name, ID, etc.) and returns a wide dataframe consisting of
 #' a single row per employee and their reporting hierarchy in a wide format.
-#' @param ee A value representing an employee
-#' @param supv A value representing the employee's supervisor
-#' @import dplyr data.tree
+#' @param ee A list of values representing employees
+#' @param supv A list of values representing the supervisors of the employees
+#' @import data.tree
 #' @export
 hierarchyWide = function(ee,supv){
-  require(dplyr)
   require(data.tree)
   if(class(ee)!=class(supv)){
     stop("Employee and supervisor inputs are different data types.")
   }else if(length(ee)!=length(supv)){
     stop("Employee and supervisor inputs are of different lengths.")
   }else{
+    df = data.frame(ee,supv,stringsAsFactors=F)
     tree = FromDataFrameNetwork(df)
     lev = max(print(tree,by="level")[,2])
     for(i in 3:lev){
@@ -26,12 +26,12 @@ hierarchyWide = function(ee,supv){
       }
     }
 
-    df.new = t(apply(df,1,function(x){c(x[is.na(x)],x[!is.na(x)])})) %>%
-      data.frame(cbind(df$ee,.),stringsAsFactors=F)
+    df.new = t(apply(df,1,function(x){c(x[is.na(x)],x[!is.na(x)])}))
+    df.new = data.frame(cbind(df$ee,df.new),stringsAsFactors=F)
     for(i in 2:ncol(df.new)){
       df.new[,i] = ifelse(df.new[,i]==df.new$X1,NA,df.new[,i])
     }
-    df.new = df.new %>% select_if(~!all(is.na(.)))
+    df.new = df.new[colSums(!is.na(df.new)) > 0]
     df.new = cbind(df.new[1],rev(df.new[2:ncol(df.new)]))
     colnames(df.new)[2:ncol(df.new)] = paste0("Supv",seq(1,ncol(df.new)-1))
     colnames(df.new)[1] = "Employee"
