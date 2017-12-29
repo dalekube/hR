4e3fe179b41bcafd32571c25bd0aeae93985c5b8
dalekube <dkube@uwalumni.com>
2017-12-28 19:10:48 -0600

    error fix
    error fix

--- R/hierarchyWide.R
+++ R/hierarchyWide.R
@@ -27,20 +27,21 @@ hierarchyWide = function(ee,supv){
     df = data.frame(ee,supv,stringsAsFactors=F)
     tree = FromDataFrameNetwork(df)
     lev = max(print(tree,by="level")[,2])
-    for(i in 3:lev){
-      df[,i] = ""
-    }
-    for(w in 3:lev){
-      for(i in 1:nrow(df)){
-        x = df$supv[df$ee==df[i,w-1]]
-        df[i,w] = ifelse(length(x)>0,x,NA)
+    if(lev>2){
+      for(i in 3:lev){
+        df[,i] = ""
+      }
+      for(w in 3:lev){
+        for(i in 1:nrow(df)){
+          x = df$supv[df$ee==df[i,w-1]]
+          df[i,w] = ifelse(length(x)>0,x,NA)
+        }
       }
     }
-
     df.new = t(apply(df,1,function(x){c(x[is.na(x)],x[!is.na(x)])}))
     df.new = data.frame(cbind(df$ee,df.new),stringsAsFactors=F)
     for(i in 2:ncol(df.new)){
-      df.new[,i] = ifelse(df.new[,i]==df.new$X1,NA,df.new[,i])
+      df.new[,i] = ifelse(df.new[,i]==df.new$V1,NA,df.new[,i])
     }
     df.new = df.new[colSums(!is.na(df.new)) > 0]
     df.new = cbind(df.new[1],rev(df.new[2:ncol(df.new)]))
