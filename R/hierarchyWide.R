ca7af3c64715e72a8b16f6e2c0524aeea3d07c8b
dalekube <dkube@uwalumni.com>
2019-01-05 21:01:15 -0600

    Performance Improv. & IDs to '@hR.com'

--- R/hierarchyWide.R
+++ R/hierarchyWide.R
@@ -12,45 +12,49 @@
 #' @export
 #' @return data frame
 #' @examples
-#' ee = c("Dale","Bob","Jill","Mark","Julie","Andrea","Susan")
-#' supv = c("Julie","Julie","Julie","Andrea","Susan","Susan","George")
+#' ee = c("Dale@hR.com","Bob@hR.com","Jill@hR.com","Mark@hR.com","Julie@hR.com","Andrea@hR.com","Susan@hR.com")
+#' supv = c("Julie@hR.com","Julie@hR.com","Julie@hR.com","Andrea@hR.com","Susan@hR.com","Susan@hR.com","George@hR.com")
 #' hierarchyWide(ee,supv)
 
 hierarchyWide = function(ee,supv){
-
+  
   # Validate character type for inputs
   if(is.factor(ee)) ee = as.character(ee)
   if(is.factor(supv)) supv = as.character(supv)
-
+  
   # Ensure the inputs are the same type
   if(class(ee)!=class(supv)){
-
+    
     stop("Employee and supervisor inputs are different data types.")
-
+    
   # Ensure the inputs are of equal length
   }else if(
     sum(is.na(ee)) > 0 |
     sum(is.na(supv)) >0
   ){
-
+    
     stop("Missing values exist.")
-
+    
   # Ensure the inputs are of equal length
   }else if(length(ee)!=length(supv)){
-
+    
     stop("Employee and supervisor inputs are of different lengths.")
-
-    }else{
-
+    
+  }else{
+    
     df = data.frame(ee,supv,stringsAsFactors=F)
-
-    tryCatch(
-      {tree = FromDataFrameNetwork(df)},
+    
+    tryCatch({tree = FromDataFrameNetwork(df)},
+             
       error=function(cond){
+        
         message("The network is not a tree! Make sure the data reflects complete, unbroken tree of employees and supervisors.")
+        
       },
+      
       finally={
-
+        
+        # Traverse the tree and create a full table
         if(tree$height>2){
           x = 3:tree$height
           df[,x] = ""
@@ -61,26 +65,24 @@ hierarchyWide = function(ee,supv){
             }
           }
         }
-        df.names = df$ee
+        
+        # Shift rows with NAs to vertically align hierarchy values
         df = t(apply(df,1,function(x){c(x[is.na(x)],x[!is.na(x)])}))
-        df = as.data.frame(
-          cbind(df.names,df),
-          stringsAsFactors=F
-          )
-
+        df = as.data.frame(cbind(ee,df),stringsAsFactors=F)
+        
         # Remove supervisor names that equal the employee name
         n = ncol(df)
-        df[2:n] = lapply(df[2:n],function(x) ifelse(x==df[,1],NA,x))
-        df = df[colSums(!is.na(df)) > 0]
-
+        df[2:n] = lapply(df[2:n],function(x) ifelse(x==ee,NA,x))
+        df = df[colSums(!is.na(df))>0]
+        
         # Reorder and set column names
         df = cbind(df[1],rev(df[2:ncol(df)]))
         supv.cols = paste0("Supv",seq(1,ncol(df)-1))
         colnames(df) = c("Employee",supv.cols)
         return(df)
-
+        
       }
     )
-
+    
   }
 }
