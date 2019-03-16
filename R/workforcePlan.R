63579b08d07fb465017a5d6e72148e40caa00b63
Dale Kube <dkube@uwalumni.com>
2019-03-16 12:17:03 -0500

    Load into default browser
    Change default setting to load app in user's default browser (e.g. Chrome) when app is launched. Included a parameter to override this behavior if the user wants to launch the app in RStudio, etc.

--- R/workforcePlan.R
+++ R/workforcePlan.R
@@ -4,6 +4,7 @@
 #' team leaders to execute basic workforce planning tasks and plan ahead for hiring, turnover, and other
 #' factors that influence a team's talent structure. Data analysts can use this alongside
 #' team leaders to convey change and proactively think about recruitment, etc.
+#' @param launch.browser Logical; whether the app should launch in the user's default browser
 #' @import shiny
 #' @import rhandsontable
 #' @import data.table
@@ -12,12 +13,13 @@
 #' @importFrom shinyjs useShinyjs show hide removeClass addClass hidden html
 #' @export
 
-workforcePlan = function(){
+workforcePlan = function(launch.browser=T){
   
   appDir = system.file("workforcePlanApp",package="hR")
   if(appDir==""){
     stop("Could not find example directory. Try re-installing `hR`.", call. = FALSE)
   }
   
-  runApp(appDir,display.mode="normal")
+  # Launch the app in the user's default browser
+  runApp(appDir,display.mode="normal",launch.browser=launch.browser)
 }
