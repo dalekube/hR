b546868fad9751e64a5cbfa86cdb6079e31560c4
Dale <dkube@uwalumni.com>
2019-05-22 23:39:07 -0500

    leverage data.table
    Convert output format to data.table, convert all incoming vectors to character to standardize ID expectations, and always print result.

--- R/hierarchyLong.R
+++ R/hierarchyLong.R
@@ -1,11 +1,11 @@
 
 #' @title hierarchyLong
 #' @description The hierarchyLong function transforms a standard set of unique employee and
-#' supervisor identifiers (employee IDs, email addresses, etc.) into a long format that can be 
-#' used to aggregate employee data by a particular line of leadership (i.e. include everyone who 
-#' rolls up to Susan). The function returns a long data frame consisting of one row per employee 
+#' supervisor identifiers (employee IDs, email addresses, etc.) into a long format that can be
+#' used to aggregate employee data by a particular line of leadership (i.e. include everyone who
+#' rolls up to Susan). The function returns a long data frame consisting of one row per employee
 #' for every supervisor above them, up to the top of the tree (i.e. the CEO in your organization).
-#' The levels represent the number of supervisors from the employee (starting with "1" for an 
+#' The levels represent the number of supervisors from the employee (starting with "1" for an
 #' employee's direct supervisor).
 #' @param ee An array containing unique identifers for employees.
 #' @param supv An array containing unique identifiers for supervisors. These values should be
@@ -19,30 +19,31 @@
 #' hierarchyLong(ee,supv)
 
 hierarchyLong = function(ee,supv){
-  
-  if(is.factor(ee)) ee = as.character(ee)
-  if(is.factor(supv)) supv = as.character(supv)
-  
+
+  # Convert factors to character
+  ee = as.character(ee)
+  supv = as.character(supv)
+
   # Ensure the inputs are the same type
   if(class(ee)!=class(supv)){
-    
+
     stop("Employee and supervisor inputs are different data types.")
-    
+
     # Ensure the inputs are of equal length
   }else if(
     sum(is.na(ee)) > 0 |
     sum(is.na(supv)) >0
   ){
-    
+
     stop("Missing values exist.")
-    
+
     # Ensure the inputs are of equal length
   }else if(length(ee)!=length(supv)){
-    
+
     stop("Employee and supervisor inputs are of different lengths.")
-    
+
   }else{
-    
+
     df = data.frame(ee,supv,stringsAsFactors=F)
     tryCatch(
       {tree = FromDataFrameNetwork(df)},
@@ -63,12 +64,14 @@ hierarchyLong = function(ee,supv){
         z = 2:ncol(df)
         colnames(df)[z] = z-1
         df = as.data.table(df)
-        df = melt.data.table(df,id.vars=1)
+        df = df[,lapply(.SD,as.character)]
+        df = melt.data.table(df,id.vars=1,variable.factor=F)
         colnames(df) = c("Employee","Level","Supervisor")
         df = df[!is.na(df$Supervisor)]
         df = df[order(df$Employee,df$Level)]
-        df = as.data.frame(df)
-        return(df)
+        df[,Level:=as.integer(Level)]
+        return(df[])
+
       }
     )
   }
