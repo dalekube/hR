f1450f7215ba778a902bf8a1ca18e755d77a9b33
Dale <dkube@uwalumni.com>
2019-10-10 19:24:49 -0500

    Improve if statements and commenting

--- R/hierarchyLong.R
+++ R/hierarchyLong.R
@@ -23,7 +23,7 @@ hierarchyLong = function(ee,supv){
   # Supress CRAN check for 'no visible binding' error
   Level = NULL
 
-  # Convert factors to character
+  # Coerce to character data type
   ee = as.character(ee)
   supv = as.character(supv)
 
@@ -31,51 +31,62 @@ hierarchyLong = function(ee,supv){
   if(class(ee)!=class(supv)){
 
     stop("Employee and supervisor inputs are different data types.")
-
-    # Ensure the inputs are of equal length
-  }else if(
-    sum(is.na(ee)) > 0 |
-    sum(is.na(supv)) >0
-  ){
+    
+  }
+  
+  # Point out NA values
+  if(sum(is.na(ee)) > 0 | sum(is.na(supv)) >0){
 
     stop("Missing values exist.")
 
-    # Ensure the inputs are of equal length
-  }else if(length(ee)!=length(supv)){
+  }
+  
+  # Ensure the inputs are of equal length
+  if(length(ee)!=length(supv)){
 
     stop("Employee and supervisor inputs are of different lengths.")
 
-  }else{
+  }
 
-    df = data.frame(ee,supv,stringsAsFactors=F)
-    tryCatch(
-      {tree = FromDataFrameNetwork(df)},
-      error=function(cond){
-        message("The network is not a tree! Make sure the data reflects complete, unbroken tree of employees and supervisors.")
-      },
-      finally={
-        if(tree$height>2){
-          x = 3:tree$height
-          df[,x] = ""
-          for(w in x){
-            for(i in 1:nrow(df)){
-              y = df$supv[df$ee==df[i,w-1]]
-              df[i,w] = ifelse(length(y)>0,y,NA)
-            }
+  df = data.frame(ee,supv,stringsAsFactors=F)
+  tryCatch(
+    {tree = FromDataFrameNetwork(df)},
+    
+    error=function(cond){
+      
+      message("The network is not a tree! Make sure the data reflects complete, unbroken tree of employees and supervisors.")
+    
+    },
+    finally={
+      
+      if(tree$height>2){
+        
+        x = 3:tree$height
+        df[,x] = ""
+        for(w in x){
+          for(i in 1:nrow(df)){
+            y = df$supv[df$ee==df[i,w-1]]
+            df[i,w] = ifelse(length(y)>0,y,NA)
           }
         }
+        
         z = 2:ncol(df)
         colnames(df)[z] = z-1
-        df = as.data.table(df)
-        df = df[,lapply(.SD,as.character)]
+        setDT(df)
         df = melt.data.table(df,id.vars=1,variable.factor=F)
-        colnames(df) = c("Employee","Level","Supervisor")
+        setnames(df,c("Employee","Level","Supervisor"))
         df = df[!is.na(df$Supervisor)]
-        df = df[order(df$Employee,df$Level)]
+        setorder(df,Employee,Level)
         df[,Level:=as.integer(Level)]
         return(df[])
-
+        
+      }else{
+        
+        return(df[])
+        
       }
-    )
-  }
+
+    }
+  )
+  
 }
--- R/hierarchyWide.R
+++ R/hierarchyWide.R
@@ -28,61 +28,67 @@ hierarchyWide = function(ee,supv){
 
     stop("Employee and supervisor inputs are different data types.")
 
-  # Ensure the inputs are of equal length
-  }else if(
-    sum(is.na(ee)) > 0 |
-    sum(is.na(supv)) >0
-  ){
+  }
+  
+  # Point out NA values
+  if(sum(is.na(ee)) > 0 | sum(is.na(supv)) >0){
 
     stop("Missing values exist.")
-
+    
+  }
+  
   # Ensure the inputs are of equal length
-  }else if(length(ee)!=length(supv)){
+  if(length(ee)!=length(supv)){
 
     stop("Employee and supervisor inputs are of different lengths.")
 
-  }else{
+  }
 
-    df = data.frame(ee,supv,stringsAsFactors=F)
+  df = data.frame(ee,supv,stringsAsFactors=F)
 
-    tryCatch({tree = FromDataFrameNetwork(df)},
+  tryCatch({tree = FromDataFrameNetwork(df)},
 
-      error=function(cond){
+    error=function(cond){
 
-        message("The network is not a tree! Make sure the data reflects complete, unbroken tree of employees and supervisors.")
+      message("The network is not a tree. Validate the data and ensure it reflects a complete, unbroken tree of employees and supervisors.")
 
-      },
+    },
 
-      finally={
+    finally={
 
-        # Traverse the tree and create a full table
-        if(tree$height>2){
-          x = 3:tree$height
-          df[,x] = ""
-          for(w in x){
-            for(i in 1:nrow(df)){
-              y = df$supv[df$ee==df[i,w-1]]
-              df[i,w] = ifelse(length(y)>0,y,NA)
-            }
+      # Traverse the tree and create a full table
+      if(tree$height>2){
+        
+        x = 3:tree$height
+        df[,x] = ""
+        for(w in x){
+          for(i in 1:nrow(df)){
+            y = df$supv[df$ee==df[i,w-1]]
+            df[i,w] = ifelse(length(y)>0,y,NA)
           }
         }
-
+        
         # Shift rows with NAs to vertically align hierarchy values
         df = t(apply(df,1,function(x){c(x[is.na(x)],x[!is.na(x)])}))
         df = data.table(cbind(ee,df))
-
+        
         # Remove supervisor names that equal the employee name
         df = cbind(df[,ee],df[,lapply(.SD,function(x) ifelse(x==ee,NA,x)),.SDcols=!"ee"])
         df = df[,colSums(!is.na(df))>0,with=F]
-
+        
         # Reorder and set column names
         df = cbind(df[,(1),with=F],rev(df[,(2:ncol(df)),with=F]))
         supv.cols = c("Employee",paste0("Supv",seq(1,ncol(df)-1)))
         setnames(df,supv.cols)
         return(df[])
-
+        
+      }else{
+        
+        return(df[])
+        
       }
-    )
 
-  }
+    }
+  )
+  
 }
